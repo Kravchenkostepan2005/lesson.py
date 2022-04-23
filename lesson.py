f46b39eaf1a4b1f1578e02c2c8302fdd1cf50905
Kravchenkostepan2005 <101333763+Kravchenkostepan2005@users.noreply.github.com>
2022-04-23 11:40:55 +0200

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,35 +1,12 @@
-import random
-
-def square_num(nums):
-    for i in nums:
-        yield i * i
-
-nums = square_num([i * i for i in range(0, 100000, 3)])
-print(nums)
-
-for element in nums:
-    print(element)
-
-f = open("abc2.txt", "w")
-for i in nums:
-    f.write(i * i + '\n')
-f.close()
-
-
-
-def square_num(nums):
     for i in nums:
         yield i * i
 
 nums = square_num([i * i for i in range(0, 100000, 5)])
 print(nums)
 
-for element in nums:
-    print(element)
-
-f = open("abc2.txt", "w")
+f = open("abc1.txt", "w")
 for i in nums:
-    f.write(i * i + '\n')
+    f.write(str(i * i) + '\n')
 f.close()
 
 
@@ -40,14 +17,12 @@ def square_num(nums):
 nums = square_num([i * i for i in range(0, 100000, 2)])
 print(nums)
 
-for element in nums:
-    print(element)
-
-f = open("abc2.txt", "w")
+f = open("abc3.txt", "w")
 for i in nums:
-    f.write(i * i + '\n')
+    f.write(str(i * i) + '\n')
 f.close()
 
+#2
 def randomizer(a):
     a = random.randint(0, 100)
     b = random.randint(0, 100)
@@ -57,22 +32,20 @@ def randomizer(a):
             break
     yield a
 
-
+#3
 def generator(start, end, step):
-    start = int(input("Enter start: "))
-    end = int(input("Enter end: "))
-    step = int(input("Enter step: "))
-    if start > end:
-        print("Wrong input")
-    if start < end:
-        yield [start, (start + step), (start + 2 * step), (start + 3 * step)]
+    list_of_values = []
+    i = start / step
+    while start - step <= i <= end:
+        i *= step
+        list_of_values.append(i)
+        print(i)
+generator(2, 128, 4)
 
+#4
+def password():
+    password = random.choice('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ$#%^&*!@')
+    password + str(random.randint(0, 9))
+    password_list = [1, 2, 3, 4, "f", "j", "F", "L", "D", "#", "$"]
 
-import random
-def password(pas):
-    for i in range(1000):
-        pas = ''
-        for x in range(16):
-            pas = pas + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
-            print('Hello, ', num, 'your password is: ', pas)
-            yield pas
+    yield password
